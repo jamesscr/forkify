460fa6b940d0c254d60e6b3a413198e7e0aa2ee1
James Allan Jean-Jacques <allanjeanjacques@hotmail.com>
2021-06-01 14:04:20 -0400

    update addRecipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,5 @@
 import * as model from './model.js';
-import {MODAL_CLOSE_SEC} from './config.js';
+import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
@@ -11,9 +11,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -21,39 +18,38 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
-    // 0. Update results view to mark selected search result active
+    // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
-    
 
-    // 1. Loading Recipe
+    // 1) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
+    // 2) Loading recipe
     await model.loadRecipe(id);
 
-    //  2. rendering Recipe
+    // 3) Rendering recipe
     recipeView.render(model.state.recipe);
-    // TEST
-    // Updating bookmarkview
-    
-    bookmarksView.update(model.state.bookmarks);
   } catch (err) {
     recipeView.renderError();
     console.error(err);
   }
 };
+
 const controlSearchResults = async function () {
   try {
     resultsView.renderSpinner();
 
-    // 1. Get search queries
+    // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
-    // 2. Load search results
+    // 2) Load search results
     await model.loadSearchResults(query);
 
-    // 3. Render results
+    // 3) Render results
     resultsView.render(model.getSearchResultsPage());
 
-    // 4. Render initial pagination buttons
+    // 4) Render initial pagination buttons
     paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
@@ -61,69 +57,67 @@ const controlSearchResults = async function () {
 };
 
 const controlPagination = function (goToPage) {
-  // 1. Render new Results
+  // 1) Render NEW results
   resultsView.render(model.getSearchResultsPage(goToPage));
 
-  // 2. Render new pagination buttons
+  // 2) Render NEW pagination buttons
   paginationView.render(model.state.search);
-}
-
-const controlServings = function (newServings) {
-  // update recipe servings(in states)
-  model.updateServings(newServings);
-
-  // update recipe view
-  // recipeView.render(model.state.recipe);
-  recipeView.update(model.state.recipe);
-
 };
 
-const controlAddBookmark=function(){
-  // Add or remove bookmark
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
-  // console.log(model.state.recipe);
-  // Update recipe view
+
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
 
-  // render Bookmarks
-  bookmarksView.render(model.state.bookmarks)
-}
-
-const controlBookmarks=function(){
+  // 3) Render bookmarks
   bookmarksView.render(model.state.bookmarks);
-}
+};
 
-const controlAddRecipe = async function(newRecipe){
-   try{
-    //  display loading spinner
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
     addRecipeView.renderSpinner();
 
-    //  Upload New Recipe
+    // Upload the new recipe data
     await model.uploadRecipe(newRecipe);
     console.log(model.state.recipe);
 
-    // Rendering recipe
+    // Render recipe
     recipeView.render(model.state.recipe);
 
-    // Success Message
+    // Success message
     addRecipeView.renderMessage();
 
-    // render BookmarkView
-    addRecipeView.render(model.state.bookmarks);
+    // Render bookmark view
+    bookmarksView.render(model.state.bookmarks);
 
-    // Change ID url
-    window.history.pushState(null, '', `#${model.state.recipe.id}`);//window.history.back();
+    // Change ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
 
-    // close form
-    setTimeout(function(){
-      addRecipeView.toggleWindow()
-    },MODAL_CLOSE_SEC * 1000);
-   }catch(err){
-     console.error('✨',err);
-     addRecipeView.renderError(err.message)
-   }
-}
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error('💥', err);
+    addRecipeView.renderError(err.message);
+  }
+};
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -133,5 +127,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-}
+};
 init();
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -1,27 +1,36 @@
-import icons from 'url:../../img/icons.svg'; // parcel 2
+import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 export default class View {
   _data;
 
-  render(data,render=true) {
-    if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author James Allan
+   * @todo Finish implementation
+   */
+  render(data, render = true) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
 
     this._data = data;
     const markup = this._generateMarkup();
-if (!render) return markup;
+
+    if (!render) return markup;
 
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
-  update(data) {
-    if (!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
 
+  update(data) {
     this._data = data;
     const newMarkup = this._generateMarkup();
 
-    // virtual dam as of reat dom!
-    const newDom = document.createRange().createContextualFragment(newMarkup);
-    const newElements = Array.from(newDom.querySelectorAll('*'));
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
     const curElements = Array.from(this._parentElement.querySelectorAll('*'));
 
     newElements.forEach((newEl, i) => {
@@ -29,60 +38,65 @@ if (!render) return markup;
       // console.log(curEl, newEl.isEqualNode(curEl));
 
       // Updates changed TEXT
-      if (!newEl.isEqualNode(curEl) && newEl.firstChild?.nodeValue.trim() !== '') {
-        // console.log('🎇✨🎇', newEl.firstChild.nodeValue.trim());
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        // console.log('💥', newEl.firstChild.nodeValue.trim());
         curEl.textContent = newEl.textContent;
       }
 
-      // Update changed ATTRIBUTES
+      // Updates changed ATTRIBUES
       if (!newEl.isEqualNode(curEl))
-        // console.log(newEl.attributes);
-        // console.log(Array.from(newEl.attributes));
-        Array.from(newEl.attributes).forEach(attr => curEl.setAttribute(attr.name, attr.value));
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
     });
-  };
+  }
+
   _clear() {
     this._parentElement.innerHTML = '';
   }
 
   renderSpinner() {
-    const markup = `<div class="spinner">
-          <svg>
-            <use href="${icons}#icon-loader"></use>
-          </svg>
-        </div>
-        `;
+    const markup = `
+      <div class="spinner">
+        <svg>
+          <use href="${icons}#icon-loader"></use>
+        </svg>
+      </div>
+    `;
     this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup)
-  };
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 
   renderError(message = this._errorMessage) {
     const markup = `
-        <div class="error">
-            <div>
-              <svg>
-                <use href="${icons}#icon-alert-triangle"></use>
-              </svg>
-            </div>
-            <p>${message}</p>
-          </div>
-        `
+      <div class="error">
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
     this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup)
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
   renderMessage(message = this._message) {
     const markup = `
-        <div class="message">
-            <div>
-              <svg>
-                <use href="${icons}#icon-smile"></use>
-              </svg>
-            </div>
-            <p>${message}</p>
-          </div>
-        `
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
     this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup)
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 }
